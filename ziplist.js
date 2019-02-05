764ab6be1d5dfeaefede77fc35fd31f229b9c90b
Quinne Uchida <qauchida@hawaii.edu>
2019-02-04 23:06:02 -1000

    WOD Attempt #2

--- ziplist.js
+++ ziplist.js
@@ -1,23 +1,19 @@
 //E27 WOD
-
 function ziplist(listOne, listTwo){
-  let i = 0;
-  let result  = [];
-  for(i =0; i<listOne.length; i++){
+  let result = [];
+  for(let i = 0; i<listOne.length; i++){
     result.push(listOne[i]);
     result.push(listTwo[i]);
   }
   return result;
 }
-
 function zipListTheSimpleWay(listOne, listTwo){
+  let result = _.flatten(_.zip(listOne,listTwo));
 
-  let merged = _.union(listOne, listTwo);
-  return merged;
+  return result;
 }
-
 let letters = ['a','b','c'];
 let numbers = [1,2,3];
+
 console.log(ziplist(letters, numbers));
 console.log(zipListTheSimpleWay(letters, numbers));
-//couldn't get it to return in order
